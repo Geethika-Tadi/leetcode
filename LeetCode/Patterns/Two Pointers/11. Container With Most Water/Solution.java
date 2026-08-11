f0736b3b14ecd4a8155a3a65d4ea5c04737e8b8c
TADI GEETHIKA SATYAPRIYA <tadigeethikasatyapriya@gmail.com>
2026-08-11 05:58:29 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.java
+++ LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.java
@@ -1,15 +1,28 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxWater = 0;
-        
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxArea = 0;
+
         while (left < right) {
-            int water = Math.min(height[left], height[right]) * (right - left);
-            maxWater = Math.max(maxWater, water);
-            
-            if (height[left] < height[right]) left++;
-            else right--;
+
+            int width = right - left;
+
+            int h = Math.min(height[left], height[right]);
+
+            int area = width * h;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        return maxWater;
+
+        return maxArea;
     }
 }
