06cd5578d1ef55556c9e63471c2a0af5f962e122
TADI GEETHIKA SATYAPRIYA <tadigeethikasatyapriya@gmail.com>
2026-08-11 06:01:37 +0530

    Add Java Solution to Patterns/Arrays

---
+++ LeetCode/Patterns/Arrays/1528. Kids With the Greatest Number of Candies/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxcandies=0;
+        for(int candy:candies)
+        {
+            maxcandies=Math.max(maxcandies,candy);
+        }
+        List <Boolean> result=new ArrayList<>();
+        for(int candy:candies){
+            if(candy+extraCandies>=maxcandies)
+            {
+                result.add(true);
+            }
+            else
+            {
+                result.add(false);
+            }
+        }
+        return result;
+        
+    }
+}
