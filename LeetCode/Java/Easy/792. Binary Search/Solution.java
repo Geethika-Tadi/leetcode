915fc661c841743535836c0158afeca75fa943cd
TADI GEETHIKA SATYAPRIYA <tadigeethikasatyapriya@gmail.com>
2026-09-07 15:57:22 +0530

    Add Easy Java Solution for 792. Binary Search from LeetCode

---
+++ LeetCode/Java/Easy/792. Binary Search/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+         int len = nums.length;
+        int s = 0;
+        int e = len - 1;
+        while (s <= e) {
+            int mid = s +( e - s ) / 2;
+            if (nums[mid] < target) {
+                s=mid+1;
+            }
+            else if (nums[mid] > target) {
+                e=mid-1;
+            }
+            else if (nums[mid] == target) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
